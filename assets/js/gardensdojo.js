b99eebdb89402ac00ca0c94dea8ccd5b7d9cc67d
riccardoperotti <riccardo.perotti@hellolabs.com>
2016-01-17 19:03:26 -0500

    Initial commit

---
+++ assets/js/gardensdojo.js
@@ -0,0 +1,22 @@
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+
+function see_more(show_id, hide_id) {
+	$("#" + show_id).slideDown(function(){
+		if (hide_id) {
+			$("#" + hide_id).hide();
+		}
+	});
+}
